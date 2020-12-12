5d975ca9e99e84312867c1e5f6bc6b192e634694
robin-li <robin-li-cn@outlook.com>
2020-12-12 11:00:47 +0800

    feat: log scm time

--- ubuntu/ubuntu-export.zsh
+++ ubuntu/ubuntu-export.zsh
@@ -1,9 +1,10 @@
 ## jdkconfig 
-
-export JAVA_HOME=/usr/lib/jdk/jdk1.8
-
+export JAVA_HOME=/usr/local/lib/jdk/jdk1.8
 export JRE_HOME=${JAVA_HOME}/jre
-
 export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
-
 export PATH=.:${JAVA_HOME}/bin:$PATH
+
+
+#maven
+export M2_HOME=/usr/local/lib/apache-maven-3.6.3
+PATH=$M2_HOME/bin:$PATH
