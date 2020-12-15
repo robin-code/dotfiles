7cb21216e8c21a20ffb714e712f748b8347a5a71
robin-li <robin-li-cn@outlook.com>
2020-12-15 10:54:15 +0800

    feat: osx export jdk maven home

--- osx/osx-export.zsh
+++ osx/osx-export.zsh
@@ -1,3 +1,12 @@
 
 
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_231.jdk/Contents/Home
+export JRE_HOME=${JAVA_HOME}/jre
+export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
+export PATH=.:${JAVA_HOME}/bin:$PATH
+
+#maven
+export M2_HOME=/usr/local/Cellar/maven/3.6.3_1
+PATH=$M2_HOME/bin:$PATH
+
 export NAMESRV_ADDR=localhost:9876
