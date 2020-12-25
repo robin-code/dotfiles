0c32215b880819441b085eebaa3bbd529eb21e0d
robin-li <robin-li-cn@outlook.com>
2020-12-25 12:42:31 +0800

    fix: fix zk alias

--- centos8/centos8-export.zsh
+++ centos8/centos8-export.zsh
@@ -4,6 +4,8 @@ export JRE_HOME=${JAVA_HOME}/jre
 export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
 export PATH=.:${JAVA_HOME}/bin:$PATH
 
+##zookeeper
+export ZOOKEEPER_HOME=/usr/local/lib/apache-zookeeper-3.6.2-bin
 
 #maven
 export M2_HOME=/usr/local/lib/apache-maven-3.6.3
